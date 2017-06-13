0f9f89a3de964583a83aeb7f3b534edb052a9653
ramesh <ramesh@panaceatek.com>
2017-06-13 19:23:36 +0530

    13-06-2017

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -4,122 +4,157 @@
  * Module dependencies.
  */
 var path = require('path'),
-  passport = require('passport'),
-  db = require(path.resolve('./config/lib/sequelize'));
+    config = require(path.resolve('./config/config')),
+    passport = require('passport'),
+    db = require(path.resolve('./config/lib/sequelize'));
+var nodemailer = require('nodemailer');
+var smtpTransport = nodemailer.createTransport(config.mailer.options);
 
 // URLs for which user can't be redirected on signin
 var noReturnUrls = [
-  '/authentication/signin',
-  '/authentication/signup'
+    '/authentication/signin',
+    '/authentication/signup'
 ];
 
 /**
  * Signup
  */
 exports.signup = function (req, res) {
-  // For security measurement we remove the roles from the req.body object
-  delete req.body.roles;
+    // For security measurement we remove the roles from the req.body object
+    delete req.body.roles;
 
-  // Init Variables
-  var user = db.User.build(req.body);
-  var message = null;
+    // Init Variables
+    var user = db.User.build(req.body);
+    var message = null;
 
-  // Add missing user fields
-  user.provider = 'local';
-  user.salt = user.makeSalt();
-  user.password = user.encryptPassword(req.body.password, user.salt);
-  // Then save the user
-  user.save().then(function(){
-    req.login(user, function(err){
-      // Remove sensitive data before login
-      user.password = undefined;
-      user.salt = undefined;
+    // Add missing user fields
+    user.provider = 'local';
+    user.salt = user.makeSalt();
+    user.password = user.encryptPassword(req.body.password, user.salt);
+    // Then save the user
+    user.save().then(function(){
+        req.login(user, function(err){
+            // Remove sensitive data before login
+            user.password = undefined;
+            user.salt = undefined;
+            //send mail to registered user mail start here
+            var httpTransport = 'http://';
+            if (config.secure && config.secure.ssl === true) {
+                httpTransport = 'https://';
+            }
+            res.render(path.resolve('modules/users/server/templates/signup-email'), {
+                name: user.fullname,
+                appName: config.app.title,
+                userEmail: user.email,
+                webUrl: httpTransport + req.headers.host//,
+                // url: httpTransport + req.headers.host + '/api/auth/reset/' + token
+            }, function (err, emailHTML) {
+                // done(err, emailHTML, user);
+                //send mail if template file found
+                var mailOptions = {
+                    to: user.email,
+                    from: config.mailer.from,
+                    subject: 'Registration Successfull',
+                    html: emailHTML
+                };
+                smtpTransport.sendMail(mailOptions, function (err) {
+                    if (!err) {
+                        res.send({
+                            message: 'An email has been sent to the provided email with further instructions.'
+                        });
+                    } else {
+                        return res.status(400).send({
+                            message: 'Failure sending email'
+                        });
+                    }
+                });
+            });
+            //send mail to registered user mail end here
+            res.json(user);
+        });
+    }).catch(function(err){
+        // Remove sensitive data before login
+        user.password = undefined;
+        user.salt = undefined;
 
-      res.json(user);
+        res.render('/authentication/signup',{
+            message: message,
+            user: user
+        });
     });
-  }).catch(function(err){
-    // Remove sensitive data before login
-    user.password = undefined;
-    user.salt = undefined;
-
-    res.render('/authentication/signup',{
-      message: message,
-      user: user
-    });
-  });
 };
 
 /**
  * Signin after passport authentication
  */
 exports.signin = function (req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
-    if (err || !user) {
-      res.status(400).send(info);
-    } else {
-      // Remove sensitive data before login
-      user.password = undefined;
-      user.salt = undefined;
-
-      req.login(user, function (err) {
-        if (err) {
-          res.status(400).send(err);
+    passport.authenticate('local', function (err, user, info) {
+        if (err || !user) {
+            res.status(400).send(info);
         } else {
-          res.json(user);
+            // Remove sensitive data before login
+            user.password = undefined;
+            user.salt = undefined;
+
+            req.login(user, function (err) {
+                if (err) {
+                    res.status(400).send(err);
+                } else {
+                    res.json(user);
+                }
+            });
         }
-      });
-    }
-  })(req, res, next);
+    })(req, res, next);
 };
 
 /**
  * Signout
  */
 exports.signout = function (req, res) {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 };
 
 /**
  * OAuth provider call
  */
 exports.oauthCall = function (strategy, scope) {
-  return function (req, res, next) {
-    // Set redirection path on session.
-    // Do not redirect to a signin or signup page
-    if (noReturnUrls.indexOf(req.query.redirect_to) === -1) {
-      req.session.redirect_to = req.query.redirect_to;
-    }
-    // Authenticate
-    passport.authenticate(strategy, scope)(req, res, next);
-  };
+    return function (req, res, next) {
+        // Set redirection path on session.
+        // Do not redirect to a signin or signup page
+        if (noReturnUrls.indexOf(req.query.redirect_to) === -1) {
+            req.session.redirect_to = req.query.redirect_to;
+        }
+        // Authenticate
+        passport.authenticate(strategy, scope)(req, res, next);
+    };
 };
 
 /**
  * OAuth callback
  */
 exports.oauthCallback = function (strategy) {
-  return function (req, res, next) {
-    // Pop redirect URL from session
-    var sessionRedirectURL = req.session.redirect_to;
-    delete req.session.redirect_to;
+    return function (req, res, next) {
+        // Pop redirect URL from session
+        var sessionRedirectURL = req.session.redirect_to;
+        delete req.session.redirect_to;
 
-    passport.authenticate(strategy, function (err, user, redirectURL) {
-      if (err) {
-        return res.redirect('/authentication/signin?err=' + encodeURIComponent(err));
-      }
-      if (!user) {
-        return res.redirect('/authentication/signin');
-      }
-      req.login(user, function (err) {
-        if (err) {
-          return res.redirect('/authentication/signin');
-        }
+        passport.authenticate(strategy, function (err, user, redirectURL) {
+            if (err) {
+                return res.redirect('/authentication/signin?err=' + encodeURIComponent(err));
+            }
+            if (!user) {
+                return res.redirect('/authentication/signin');
+            }
+            req.login(user, function (err) {
+                if (err) {
+                    return res.redirect('/authentication/signin');
+                }
 
-        return res.redirect(redirectURL || sessionRedirectURL || '/');
-      });
-    })(req, res, next);
-  };
+                return res.redirect(redirectURL || sessionRedirectURL || '/');
+            });
+        })(req, res, next);
+    };
 };
 
 /**
